ce31741d2b37e620e7aec75b7c53c0297674d8e0
Jordan-Lane <JLane@uws110435usr.uws.ualberta.ca>
2016-02-05 17:59:41 -0700

    Create Graph Fn

--- Assignment1.py
+++ Assignment1.py
@@ -2,29 +2,4 @@ from graph.py import WeightedGraph
 import least_cost_path.py
 import cost_distance.py
 import sys
-
-import sys
-
-graph = WeightedGraph()
-
-filename = sys.argv[1] #Gives the first line command-line arguement
-
-with open(filename) as file:
-	vert_dict = {}
-	edge_dict = {}
-	for line in file: # Variable 'line' loops over each line in the file
-		line = line.strip() # Remove trailing newline character
-        # Process the line here
-		lineinput = line.split(",")
-
-		if lineinput[0] == 'V':
-			latitude = int(float(lineinput[2]*100000))
-			longitude = int(float(lineinput[3]*100000))
-			graph.add_vertex(int(lineinput[1]))
-			ver_dict[int(lineinput[1])] = (latitude, longitude)
-
-		elif lineinput[0] == 'E':
-			latitude = int(float(lineinput[1]*100000))
-			longitude = int(float(lineinput[2]*100000))
-			graph.add_edge(latitude, longitude)
-			edge_dict[latitude,longitude] = lineinput[3]
+filename = sys.argv[1]
---
+++ create_graph.py
@@ -0,0 +1,30 @@
+
+import cost_distance
+from graph
+
+def create_graph(filename):
+
+    graph = WeightedGraph()
+    #Gives the first line command-line arguement
+
+    with open(filename) as file:
+        vert_dict = {}
+        edge_dict = {}
+	    for line in file: # Variable 'line' loops over each line in the file
+            line = line.strip() # Remove trailing newline character
+        # Process the line here
+            lineinput = line.split(",")
+
+            if lineinput[0] == 'V':
+                latitude = int(float(lineinput[2]*100000))
+			    longitude = int(float(lineinput[3]*100000))
+			    graph.add_vertex(int(lineinput[1]))
+                ver_dict[int(lineinput[1])] = (latitude, longitude)
+
+		    elif lineinput[0] == 'E':
+			    point1 = vert_dict[int(lineinput[1])]
+			    point2 = vert_dict[int(lineinput[2])]
+			    weight = cost_distance(point1,point2)
+
+                graph.add_edge(lineinput[1],lineinput[2],weight)
+                edge_dict[(point1,point2)] = lineinput[3]
